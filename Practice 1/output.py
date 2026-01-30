abc4718fd8c2526d18864eb7f2250f0b70224ce6
aru17n <n.aruzhan070@gamil.com>
2026-01-31 03:19:28 +0500

    -

---
+++ Practice 1/output.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+#Double Quotes
+print("This will work!")
+print('This will also work!')
+
+#Print without a new line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+#Print Numbers
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
