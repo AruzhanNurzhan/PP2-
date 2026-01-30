ff3e543a8a38bb51e022836cd5403aa47a0e6748
aru17n <n.aruzhan070@gamil.com>
2026-01-30 22:17:22 +0500

    -

--- Practice 1/variables.py
+++ Practice 1/variables.py
@@ -70,3 +70,42 @@ x = "Python "
 y = "is "
 z = "awesome"
 print(x + y + z)
+
+#Python - Global Variables
+x = "awesome"
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x) 
+
+#The global Keyword
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x) 
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x) 
