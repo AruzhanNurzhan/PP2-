750546b8a7a76054fd60ef541f9af9b32a1027f3
aru17n <n.aruzhan070@gamil.com>
2026-03-27 20:45:36 +0500

    -

--- Practice 7/connect.py
+++ Practice 7/connect.py
@@ -10,9 +10,9 @@ def init_db():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS phonebook (
             id SERIAL PRIMARY KEY,
-            first_name VARCHAR(50) NOT NULL,
-            last_name VARCHAR(50),
-            phone VARCHAR(20) UNIQUE NOT NULL
+            first_name  NOT NULL,
+            last_name TEXT,
+            phone x UNIQUE NOT NULL
         )
     """)
     conn.commit()
