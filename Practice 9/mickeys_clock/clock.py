3e7aabcbf5c93253aa002908c92b272f0750abfc
aru17n <n.aruzhan070@gamil.com>
2026-04-18 09:54:49 +0500

    fix clock

--- Practice 9/mickeys_clock/clock.py
+++ Practice 9/mickeys_clock/clock.py
@@ -4,6 +4,7 @@ import os
 
 
 class Clock:
+
     def __init__(self):
 
         BASE_DIR = os.path.dirname(__file__)
@@ -11,11 +12,9 @@ class Clock:
 
         self.clock_img = pygame.image.load(os.path.join(IMG_DIR, "clock.png"))
 
-        # две стрелки (если у тебя одна — можно временно копировать файл)
         self.minute_hand = pygame.image.load(os.path.join(IMG_DIR, "rightarm.png"))
         self.second_hand = pygame.image.load(os.path.join(IMG_DIR, "leftarm.png"))
 
-        # фикс ориентации pygame (очень важно)
         self.base_fix = -90
 
     def get_time_angles(self):
@@ -25,7 +24,6 @@ class Clock:
         minutes = now.minute
         seconds = now.second
 
-        # только минуты и секунды (как в задании!)
         minute_angle = -6 * minutes
         second_angle = -6 * seconds
 
@@ -35,19 +33,16 @@ class Clock:
 
         center = screen.get_rect().center
 
-        # фон часов
         clock_rect = self.clock_img.get_rect(center=center)
         screen.blit(self.clock_img, clock_rect)
 
         minute_angle, second_angle = self.get_time_angles()
 
-        # минутная стрелка (правая рука)
         minute_img = pygame.transform.rotate(
             self.minute_hand,
             minute_angle + self.base_fix
         )
 
-        # секундная стрелка (левая рука)
         second_img = pygame.transform.rotate(
             self.second_hand,
             second_angle + self.base_fix
